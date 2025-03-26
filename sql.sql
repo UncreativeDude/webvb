94d66c2b48a9b74ab68a85d3c32369a99b39b64a
UncreativeDude <michalakp685@gmail.com>
2025-03-26 15:01:20 +0100

    main

--- sql.sql
+++ sql.sql
@@ -7,7 +7,6 @@ CREATE TABLE uzytkowniczy {
 
 CREATE TABLE maszyny {
     id INT AUTO-INCREMENT PRIMARY KEY,
-    login VARCHAR(50) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    id_uzytkownika INT NOT NULL,
+    nazwaMaszyny VARCHAR(50) NOT NULL,
 }
