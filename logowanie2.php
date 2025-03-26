3af34426bd9b2fbb0ff741d49028629bd609e4eb
UncreativeDude <michalakp685@gmail.com>
2025-03-26 14:53:53 +0100

    main

--- logowanie2.php
+++ logowanie2.php
@@ -4,7 +4,9 @@
 	$haslo = hash("sha256",$_GET["pwd"]);
 	mysqli_connect('artkoc7548.mysql.dhosting.pl', 'seey3m_michalak', 'thi3eexohY3s', 'ange9a_michalak');
 	mysqli_select_db($bazaNazwa);
-	$rezultat = mysqli_query("SELECT id FROM uzytkownicy WHERE login='".$_GET["lgn"]."' AND haslo='".$haslo."'");
+	$sql = "SELECT id FROM uzytkownicy WHERE login='".$_GET["lgn"]."' AND haslo='".$haslo."'"
+	$lista22=mysqli_query($connect,$sql);
+	$rezultat = mysqli_query($connect,$sql);
 	if(mysql_num_rows($rezultat) == 1) {
 		mysqli_close();
 		setcookie('login', $_GET["lgn"], time()+3600*24);
@@ -12,7 +14,7 @@
 		header("Location: menu.php");
 		die();
 	}
-	mysqli_close();
+	mysqli_close($connect);
 	header("Location: logowanie.php");
 	die();
 ?>
