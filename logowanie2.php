b25d6f9c7a8015a17d8878c3aa16c09cdd2d0282
UncreativeDude <michalakp685@gmail.com>
2025-03-26 15:09:19 +0100

    main

--- logowanie2.php
+++ logowanie2.php
@@ -4,7 +4,7 @@
 	$haslo = hash("sha256",$_GET["pwd"]);
 	mysqli_connect('artkoc7548.mysql.dhosting.pl', 'seey3m_michalak', 'thi3eexohY3s', 'ange9a_michalak');
 	mysqli_select_db($bazaNazwa);
-	$sql = "SELECT id FROM uzytkownicy WHERE login='".$_GET["lgn"]."' AND haslo='".$haslo."'"
+	$sql = "SELECT id FROM ab_uzytkownicy WHERE login='".$_GET["lgn"]."' AND haslo='".$haslo."'"
 	$lista22=mysqli_query($connect,$sql22);
 	$rezultat = mysqli_query($connect,$sql);
 	if(mysql_num_rows($rezultat) == 1) {
--- sql.sql
+++ sql.sql
@@ -1,12 +1,12 @@
-CREATE TABLE uzytkowniczy {
-    id INT AUTO-INCREMENT PRIMARY KEY,
+CREATE TABLE ab_uzytkowniczy (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     login VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
-}
+)
 
-CREATE TABLE maszyny {
-    id INT AUTO-INCREMENT PRIMARY KEY,
-    id_uzytkownika INT NOT NULL,
+CREATE TABLE maszyny (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    idUzytkownika INT NOT NULL,
     nazwaMaszyny VARCHAR(50) NOT NULL,
-}
+)
