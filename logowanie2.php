60052fd642a73c7e089946ecfbbf82d3c764849b
UncreativeDude <michalakp685@gmail.com>
2025-03-26 14:09:50 +0100

    main

--- logowanie2.php
+++ logowanie2.php
@@ -1,18 +1,24 @@
 <?php
 	include 'konfiguracja.php';
-	
-	$haslo = hash("sha256",$_GET["pwd"]);
-	mysql_connect($bazaAdres,$bazaLogin,$basaHaslo);
-	mysql_select_db($bazaNazwa);
-	$rezultat = mysql_query("SELECT id FROM uzytkownicy WHERE login='".$_GET["lgn"]."' AND haslo='".$haslo."'");
-	if(mysql_num_rows($rezultat) == 1) {
-		mysql_close();
-		setcookie('login', $_GET["lgn"], time()+3600*24);
-		setcookie('haslo', $haslo, time()+3600*24);
+	$conn = new mysqli($bazaAdres, $bazaLogin, $basaHaslo, $bazaNazwa);
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	$login = $_GET['lgn'];
+	$haslo = hash('sha256', $_GET['pwd']);
+	$stmt = $conn->prepare("SELECT id FROM uzytkownicy WHERE login = ? AND haslo = ?");
+	$stmt->bind_param("ss", $login, $haslo);
+	$stmt->execute();
+	$stmt->store_result();
+	if ($stmt->num_rows == 1) {
+		setcookie('login', $login, time() + 3600 * 24, "/");
+		setcookie('haslo', $haslo, time() + 3600 * 24, "/");
 		header("Location: menu.php");
 		die();
+	} else {
+		header("Location: logowanie.php");
+		die();
 	}
-	mysql_close();
-	header("Location: logowanie.php");
-	die();
+	$stmt->close();
+	$conn->close();
 ?>
