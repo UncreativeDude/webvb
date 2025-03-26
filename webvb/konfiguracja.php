5d8dd19819273c44c87ec4865bc9c32ae07ee7bc
UncreativeDude <michalakp685@gmail.com>
2025-03-26 12:38:42 +0100

    Delete webvb directory

--- webvb/konfiguracja.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$bazaAdres = "localhost";
-	$bazaLogin = "root";
-	$basaHaslo = "1234";
-	$bazaNazwa = "vbox";
-	$prawidloweHaslo = hash("sha256","1234");
-?>
--- webvb/wyloguj.php
+++
@@ -1,7 +0,0 @@
-<?php
-	setcookie('login', NULL, time()-1);
-	setcookie('haslo', NULL, time()-1);
-	setcookie('hasloROOT', NULL, time()-1);
-	header("Location: index.php");
-	die();
-?>
